70f48a6c88bf50ac54353ab42a71fd1ef0fe7936
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-12-18 10:03:53 +0100

    Add TrieNode tree type.

---
+++ src/Node/TrieNode.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace drupol\phptree\Node;
+
+/**
+ * Class TrieNode
+ */
+class TrieNode extends KeyValueNode
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function add(NodeInterface ...$nodes): NodeInterface
+    {
+        foreach ($nodes as $node) {
+            $data = $node->getValue();
+
+            $hash = \hash('sha256', $node->getKey() . $data);
+
+            $node = new TrieNode($hash, \substr($data, 0, 1));
+            $parent = $this->append($node);
+
+            $dataWithoutFirstLetter = \substr($data, 1);
+            if (!empty($dataWithoutFirstLetter)) {
+                $parent->add(new TrieNode($hash, $dataWithoutFirstLetter));
+            } else {
+                $nodes = [$node->getValue()];
+                foreach ($node->getAncestors() as $ancestor) {
+                    $nodes[] = $ancestor->getValue();
+                }
+                \array_pop($nodes);
+                $node->append(new TrieNode($hash, \strrev(\implode('', $nodes))));
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param \drupol\phptree\Node\ValueNodeInterface $node
+     *
+     * @return \drupol\phptree\Node\NodeInterface|\drupol\phptree\Node\ValueNodeInterface
+     */
+    private function append(ValueNodeInterface $node)
+    {
+        /** @var \drupol\phptree\Node\ValueNodeInterface $child */
+        foreach ($this->children() as $child) {
+            /** @var \drupol\phptree\Node\ValueNodeInterface $node */
+            if ($node->getValue() === $child->getValue()) {
+                return $child;
+            }
+        }
+
+        parent::add($node);
+
+        return $node;
+    }
+}
