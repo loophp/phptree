b3bedaf76029886b5d3b8e3aebf55b7fdc7e85f2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-12-19 12:59:29 +0100

    Update phpdoc.

--- src/Exporter/Text.php
+++ src/Exporter/Text.php
@@ -12,13 +12,7 @@ use drupol\phptree\Node\ValueNodeInterface;
 class Text implements ExporterInterface
 {
     /**
-     * Export the node in a string.
-     *
-     * @param \drupol\phptree\Node\ValueNodeInterface $node
-     *   The node.
-     *
-     * @return string
-     *   The string.
+     * {@inheritdoc}
      */
     public function export(ValueNodeInterface $node): string
     {
