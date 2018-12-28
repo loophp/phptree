b6bcc23de38107e204d9ed7f30b4acf3c6eb9089
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-12-28 23:08:48 +0100

    Add an Ascii exporter and its tests.

---
+++ spec/drupol/phptree/Exporter/AsciiSpec.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace spec\drupol\phptree\Exporter;
+
+use drupol\phptree\Exporter\Ascii;
+use drupol\phptree\Node\ValueNode;
+use PhpSpec\ObjectBehavior;
+
+class AsciiSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(Ascii::class);
+    }
+
+    public function it_can_export_to_ascii()
+    {
+        $tree = new ValueNode('root', 2);
+
+        foreach (\range('A', 'Z') as $key => $value) {
+            $nodes[$value] = new ValueNode($value, 2);
+        }
+
+        $tree->add(...\array_values($nodes));
+
+        $expected = <<<EOF
+├─ root
+└─┐
+  ├─┐
+  │ ├─ A
+  │ └─┐
+  │   ├─┐
+  │   │ ├─ C
+  │   │ └─┐
+  │   │   ├─┐
+  │   │   │ ├─ G
+  │   │   │ └─┐
+  │   │   │   ├─┐
+  │   │   │   │ └─ O
+  │   │   │   └─┐
+  │   │   │     └─ P
+  │   │   └─┐
+  │   │     ├─ H
+  │   │     └─┐
+  │   │       ├─┐
+  │   │       │ └─ Q
+  │   │       └─┐
+  │   │         └─ R
+  │   └─┐
+  │     ├─ D
+  │     └─┐
+  │       ├─┐
+  │       │ ├─ I
+  │       │ └─┐
+  │       │   ├─┐
+  │       │   │ └─ S
+  │       │   └─┐
+  │       │     └─ T
+  │       └─┐
+  │         ├─ J
+  │         └─┐
+  │           ├─┐
+  │           │ └─ U
+  │           └─┐
+  │             └─ V
+  └─┐
+    ├─ B
+    └─┐
+      ├─┐
+      │ ├─ E
+      │ └─┐
+      │   ├─┐
+      │   │ ├─ K
+      │   │ └─┐
+      │   │   ├─┐
+      │   │   │ └─ W
+      │   │   └─┐
+      │   │     └─ X
+      │   └─┐
+      │     ├─ L
+      │     └─┐
+      │       ├─┐
+      │       │ └─ Y
+      │       └─┐
+      │         └─ Z
+      └─┐
+        ├─ F
+        └─┐
+          ├─┐
+          │ └─ M
+          └─┐
+            └─ N
+
+EOF;
+
+        $this
+            ->export($tree)
+            ->shouldReturn($expected);
+    }
+}
---
+++ src/Exporter/Ascii.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace drupol\phptree\Exporter;
+
+use drupol\phptree\Node\ValueNodeInterface;
+
+/**
+ * Class Ascii
+ */
+class Ascii implements ExporterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function export(ValueNodeInterface $node): string
+    {
+        $tree = new \RecursiveTreeIterator(
+            new \RecursiveArrayIterator(
+                $this->doExportAsArray($node)
+            ),
+            \RecursiveTreeIterator::BYPASS_KEY,
+            \CachingIterator::CATCH_GET_CHILD,
+            \RecursiveTreeIterator::SELF_FIRST
+        );
+
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_LEFT, '');
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_END_HAS_NEXT, '├─');
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_END_LAST, '└─');
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_RIGHT, '');
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_MID_LAST, '  ');
+        $tree->setPrefixPart(\RecursiveTreeIterator::PREFIX_MID_HAS_NEXT, '│ ');
+
+        $output = '';
+
+        foreach ($tree as $value) {
+            $entry = ('Array' === $entry = $tree->getEntry()) ?
+                    '┐':
+                    ' ' . $entry;
+
+            $output .= $tree->getPrefix() . $entry . $tree->getPostfix() . PHP_EOL;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Export the tree in an array.
+     *
+     * @return array
+     *   The tree exported into an array.
+     */
+    private function doExportAsArray(ValueNodeInterface $node): array
+    {
+        $children = [];
+        /** @var ValueNodeInterface $child */
+        foreach ($node->children() as $child) {
+            $children[] = $this->doExportAsArray($child);
+        }
+
+        return [] === $children ?
+            [$node->getValue()]:
+            [$node->getValue(), $children];
+    }
+}
