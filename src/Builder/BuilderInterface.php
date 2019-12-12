37e93ccc15eab9b8108fc825564d8d37b914d56b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-12-12 16:18:05 +0100

    Add BuilderInterface.php.

---
+++ src/Builder/BuilderInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace drupol\phptree\Builder;
+
+use drupol\phptree\Node\NodeInterface;
+
+/**
+ * Interface BuilderInterface.
+ */
+interface BuilderInterface
+{
+    /**
+     * @param iterable<NodeInterface> $nodes
+     *
+     * @return \drupol\phptree\Node\NodeInterface
+     */
+    public static function create(iterable $nodes): NodeInterface;
+}
